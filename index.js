e1c6913ea1d3fce12470de967aa58a8e62e09057
mxhs <maxwellstofman@gmail.com>
2020-09-11 17:35:23 -0700

    Halfway finished with Javascript.

--- index.js
+++ index.js
@@ -208,11 +208,15 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0].name)
+console.log(artists[2].bio)
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+artists[8].name = "Vincent Van Gogh"
 
+console.log(artists[8].name)
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -222,20 +226,36 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
+
+
 function getArtistByIndex(array, index) {
     /* code here */
+    return `The artist at index ${array[index].id} is ${array[index].name}.`
+
   }
+
+  console.log(getArtistByIndex(artists, 0))
   
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
+integer1 = Number(artists[0].years.split("-",1));
+integer2 = Number(artists[0].years.split(integer1, 1));
+console.log(integer1, integer2)
 
-  /* Code here */
 
-}
+// let artistNamesByCentury = []
+
+// function get20s(array."", ""){
+//   for(i = 0; i < array.length; i+++){
+//     if(split array[i].years("the integer") >= "1900" && split array[i].years("the second integer") <= 2000){ //or it might use the `${}` function
+//       artistNamesByCentury.push(array[i].name)
+//     } 
+//       }
+//   return 
+// }
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -248,9 +268,21 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
+
+
+
+function removeArtist(array, index) {
     /* code here */
+    if(array[index].id === index){
+      delete array[index];
+    }
+    return(array.length)
   }
+
+removeArtist(artists, 0)
+
+console.log(artists.length)
+
   
  
 
